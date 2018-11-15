79b31f144bf4857357de5ae98b8fc4991aa0c78b
elahe ar <elahe@Brugers-MacBook-Pro-2.local>
2018-11-15 20:39:20 +0100

    Updates

---
+++ precache-manifest.837ae19cf4b0445df44ff8ba8b39b7dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "b56a70327748b895742b",
+    "url": "/react-gh-pages/static/js/main.b56a7032.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-gh-pages/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "b56a70327748b895742b",
+    "url": "/react-gh-pages/static/css/main.27c740f0.chunk.css"
+  },
+  {
+    "revision": "643bb167870169ea8a1000db21d169c9",
+    "url": "/react-gh-pages/index.html"
+  }
+];
